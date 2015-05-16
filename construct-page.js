25ece413286840fe4e1aeb4fda6ef45a619cdbe7
Liam (Aqua) <liam4@users.noreply.github.com>
2015-05-16 10:48:06 -0300

    Update construct-page.js

--- construct-page.js
+++ construct-page.js
@@ -2,9 +2,11 @@
 
 // More testing 0_0
 
+var domParser = new window.DOMParser();
+
 fetch('/footer.html').then(function(response) {
   return response.text();
 }).then(function(body) {
-  var bodyDOM = window.DOMParser(body);
+  var bodyDOM = new domParser.parseFromString(body, "text/html");
   document.body.appendChild(bodyDOM);
 });
