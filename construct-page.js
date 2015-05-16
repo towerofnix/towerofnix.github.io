4643045d01ce72e1f7a1fded353718e068284bf7
Liam (Aqua) <liam4@users.noreply.github.com>
2015-05-16 10:26:11 -0300

    Update construct-page.js

--- construct-page.js
+++ construct-page.js
@@ -1,1 +1,13 @@
 // UNDER CONSTRUCTION
+
+// This is a test using include for HTML5.
+// Check it out here: http://www.html5rocks.com/en/tutorials/webcomponents/imports/
+
+// TODO Make with fetch or XMLHttpRequest for cross-browser?
+
+var links = document.getElementsByTagName("link");
+for (var l = 0; l < links.length; l++) {
+  if (links[l].rel === "import") {
+    console.log(links[l].title);
+  }
+}
