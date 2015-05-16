d6055225dd4ce464d4134b581326a6a2c9fe3ba5
Liam (Aqua) <liam4@users.noreply.github.com>
2015-05-16 10:36:22 -0300

    Update construct-page.js

--- construct-page.js
+++ construct-page.js
@@ -5,7 +5,7 @@
 
 // TODO Make with fetch or XMLHttpRequest for cross-browser?
 
-setTimeout(function() {
+window.onload = function() {
   var links = document.getElementsByTagName("link");
   for (var l = 0; l < links.length; l++) {
     if (links[l].rel === "import") {
@@ -13,4 +13,4 @@ setTimeout(function() {
       targetElement.appendChild(links[l].import);
     }
   }
-}, 100);
+};
