d0c3b16fdf0f8b51f4b7ae8b566545c53f26fbcb
Liam (Aqua) <liam4@users.noreply.github.com>
2015-05-16 10:44:03 -0300

    Update construct-page.js

--- construct-page.js
+++ construct-page.js
@@ -1,16 +1,9 @@
 // UNDER CONSTRUCTION
 
-// This is a test using include for HTML5.
-// Check it out here: http://www.html5rocks.com/en/tutorials/webcomponents/imports/
+// More testing 0_0
 
-// TODO Make with fetch or XMLHttpRequest for cross-browser?
-
-var links = document.getElementsByTagName("link");
-for (var l = 0; l < links.length; l++) {
-  if (links[l].rel === "import") {
-    links[l].addEventListener('load', function() {
-      var targetElement = document.querySelector(this.title);
-      targetElement.appendChild(this.import);
-    });
-  }
-}
+fetch('/footer.html').then(function(response) {
+  return response.text();
+}).then(function(body) {
+  document.body.appendChild(body);
+});
